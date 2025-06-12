2d60064d63d74715d20a76c2d27828a23e8eca3e
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-06-12 08:43:18 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '1.5rem',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      Hello World!
+    </div>
+  );
+}
+
+export default App; 
